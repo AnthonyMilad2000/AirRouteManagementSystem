390e207d95bce9fc4163af058901c309c9863ce5
abdalrhmanMohamedZakria <am8343@fayoum.edu.eg>
2026-01-31 16:10:07 +0200

    Update AppConfiger

--- AirRouteManagementSystem/AppConfiguration.cs
+++ AirRouteManagementSystem/AppConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using AirRouteManagementSystem.Repository.IRepository;
+﻿using AirRouteManagementSystem.Reposatory;
+using AirRouteManagementSystem.Repository.IRepository;
+using AirRouteManagementSystem.Services;
 using AirRouteManagementSystem.Utilities;
 using Microsoft.AspNetCore.Identity.UI.Services;
 
@@ -6,13 +8,26 @@ namespace AirRouteManagementSystem
 {
     public static class AppConfiguration
     {
-        public static void RegisterConfig(this IServiceCollection services) 
+        public static void RegisterConfig(this IServiceCollection services)
         {
-           // Email Sender
-            services.AddTransient<IEmailSender,EmailSender>();
+            // Email Sender
+            services.AddTransient<IEmailSender, EmailSender>();
 
             // DBInitializar
             services.AddScoped<IDBInitializar, DBInitializar>();
+
+            // Reposatoey
+            services.AddScoped<IRepository<ApplicationUserOTP>, Repository<ApplicationUserOTP>>();
+
+            // Service
+            services.AddTransient<ITokenService, TokenService>();
+            services.AddScoped<IAccountServices, AccountService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+            services.AddHttpClient();
+
+            //Open AI
+            services.AddHttpClient<IOpenAiService, OpenAiService>();
+
         }
     }
 }
