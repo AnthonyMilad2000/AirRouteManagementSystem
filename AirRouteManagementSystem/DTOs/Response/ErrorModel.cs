4e02a42dfae3eafe33860a71af57c3e56e24fa5b
abdalrhmanMohamedZakria <am8343@fayoum.edu.eg>
2026-02-01 17:27:21 +0200

    Update ErrorModel

--- AirRouteManagementSystem/DTOs/Response/ErrorModel.cs
+++ AirRouteManagementSystem/DTOs/Response/ErrorModel.cs
@@ -2,7 +2,6 @@
 {
     public class ErrorModel
     {
-        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Code { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime ReturnedAt { get; set; } = DateTime.UtcNow;
